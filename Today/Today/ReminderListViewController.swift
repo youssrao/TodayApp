8b37cd14e17d96de2a9a49c862b04e0c5e5df3c4
youssrao <youssra.outelli@gmail.com>
2022-03-05 13:58:34 +0100

    setup list config with collection view

--- Today/Today/ReminderListViewController.swift
+++ Today/Today/ReminderListViewController.swift
@@ -7,10 +7,21 @@
 
 import UIKit
 
-class ReminderListViewController: UITableViewController {
+class ReminderListViewController: UICollectionViewController {
 
+  override func viewDidLoad() {
+      super.viewDidLoad()
 
+      let listLayout = listLayout()
+      collectionView.collectionViewLayout = listLayout
+  }
 
+  //Creates a new list configuration variable with the grouped appearance
+  private func listLayout() -> UICollectionViewCompositionalLayout {
+      var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+      listConfiguration.showsSeparators = false
+      listConfiguration.backgroundColor = .clear
 
+      return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+  }
 }
-
