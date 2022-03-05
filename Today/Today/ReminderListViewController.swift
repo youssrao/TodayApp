42b16cd1e21dfb2a652e85e2892539dc396a967d
youssrao <youssra.outelli@gmail.com>
2022-03-05 15:43:28 +0100

    cell appearance + connecting to data source

--- Today/Today/ReminderListViewController.swift
+++ Today/Today/ReminderListViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
+  //Diffable data source updates and animates the user interface when the data changes
+  typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+
+  var dataSource: DataSource!
+
 
   override func viewDidLoad() {
       super.viewDidLoad()
 
       let listLayout = listLayout()
       collectionView.collectionViewLayout = listLayout
+
+    //Specifies how to configure the content and appearance of a cell
+    let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+      let reminder = Reminder.testData[indexPath.item]
+      var contentConfiguration = cell.defaultContentConfiguration()
+      contentConfiguration.text = reminder.title
+      cell.contentConfiguration = contentConfiguration
+
+
+
+    }
+
+    //Pass collection view to the data source and return cell
+    dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+      return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+
+            }
+
   }
 
   //Creates a new list configuration variable with the grouped appearance
