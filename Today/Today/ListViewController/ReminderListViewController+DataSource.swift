407c3dbf8150527ce182d327d2d76718f92b4203
youssrao <youssra.outelli@gmail.com>
2022-03-05 18:32:29 +0100

    split into data source vc and behavior vc

---
+++ Today/Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,29 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  Today
+//
+//  Created by Youssra Outelli on 05/03/2022.
+//
+
+import UIKit
+
+extension ReminderListViewController {
+  //Diffable data source updates and animates the user interface when the data changes
+  typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+  typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+
+  func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+    //Specifies how to configure the content and appearance of a cell
+      let reminder = Reminder.testData[indexPath.item]
+      var contentConfiguration = cell.defaultContentConfiguration()
+      contentConfiguration.text = reminder.title
+      contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+      contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+      cell.contentConfiguration = contentConfiguration
+
+    }
+
+
+
+  
+}
--- Today/Today/ListViewController/ReminderListViewController.swift
+++ Today/Today/ListViewController/ReminderListViewController.swift
@@ -8,10 +8,6 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
-  //Diffable data source updates and animates the user interface when the data changes
-  typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-  typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
-
   var dataSource: DataSource!
 
 
@@ -21,16 +17,7 @@ class ReminderListViewController: UICollectionViewController {
       let listLayout = listLayout()
       collectionView.collectionViewLayout = listLayout
 
-    //Specifies how to configure the content and appearance of a cell
-    let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
-      let reminder = Reminder.testData[indexPath.item]
-      var contentConfiguration = cell.defaultContentConfiguration()
-      contentConfiguration.text = reminder.title
-      cell.contentConfiguration = contentConfiguration
-
-
-
-    }
+    let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
 
     //Pass collection view to the data source and return cell
     dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
