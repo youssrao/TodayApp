ec71ddc1d9dd7f19921af0929367dfab8b095ff8
youssrao <youssra.outelli@gmail.com>
2022-03-14 21:49:17 +0100

    default snapshot for reminder detail view

--- Today/Today/DetailViewController/ReminderViewController.swift
+++ Today/Today/DetailViewController/ReminderViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ReminderViewController: UICollectionViewController {
   private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+  private typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Row>
+
 
   var reminder: Reminder
   private var dataSource: DataSource!
@@ -34,6 +36,9 @@ class ReminderViewController: UICollectionViewController {
     dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Row) in
       return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
     }
+
+    updateSnapshot()
+
   }
 
   func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
@@ -43,6 +48,14 @@ class ReminderViewController: UICollectionViewController {
     contentConfiguration.image = row.image
     cell.contentConfiguration = contentConfiguration
     cell.tintColor = UIColor(named: "TodayPrimaryTint")
+  }
+
+  private func updateSnapshot() {
+    var snapshot = Snapshot()
+    snapshot.appendSections([0])
+    snapshot.appendItems([.viewTitle, .viewDate, .viewTime, .viewNotes], toSection: 0)
+    dataSource.apply(snapshot)
+
   }
 
   func text(for row: Row) -> String? {
