b7299fc68b028e352bd174073c31ee1c9bdea499
youssrao <youssra.outelli@gmail.com>
2022-03-14 22:13:35 +0100

    setup

--- Today/Today/ListViewController/ReminderListViewController.swift
+++ Today/Today/ListViewController/ReminderListViewController.swift
@@ -13,10 +13,10 @@ class ReminderListViewController: UICollectionViewController {
 
 
   override func viewDidLoad() {
-      super.viewDidLoad()
+    super.viewDidLoad()
 
-      let listLayout = listLayout()
-      collectionView.collectionViewLayout = listLayout
+    let listLayout = listLayout()
+    collectionView.collectionViewLayout = listLayout
 
     let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
 
@@ -24,7 +24,7 @@ class ReminderListViewController: UICollectionViewController {
     dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Reminder.ID) in
       return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
 
-            }
+    }
 
     //append items and sections to snapshot and display this on screen
     var snapshot = Snapshot()
@@ -37,15 +37,26 @@ class ReminderListViewController: UICollectionViewController {
 
     collectionView.dataSource = dataSource
 
+  }
 
+  override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+    let id = reminders[indexPath.item].id
+    showDetail(for: id)
+    return false
+  }
+
+  func showDetail(for id: Reminder.ID) {
+    let reminder = reminder(for: id)
+    let viewController = ReminderViewController(reminder: reminder)
+    navigationController?.pushViewController(viewController, animated: true)
   }
 
   //Creates a new list configuration variable with the grouped appearance
   private func listLayout() -> UICollectionViewCompositionalLayout {
-      var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
-      listConfiguration.showsSeparators = false
-      listConfiguration.backgroundColor = .clear
+    var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+    listConfiguration.showsSeparators = false
+    listConfiguration.backgroundColor = .clear
 
-      return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    return UICollectionViewCompositionalLayout.list(using: listConfiguration)
   }
 }
