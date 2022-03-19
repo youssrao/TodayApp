d3411ce095ebe02e6dc862b05bbce25eaebdf6e2
youssrao <youssra.outelli@gmail.com>
2022-03-19 01:27:06 +0100

    prepare content view for editing

--- Today/Today/ContentViews/TextFieldContentView.swift
+++ Today/Today/ContentViews/TextFieldContentView.swift
@@ -8,14 +8,27 @@
 import UIKit
 
 //custom text field object that displays the title in an editable text area
-class TextFieldContentView: UIView {
-    let textField = UITextField()
+class TextFieldContentView: UIView, UIContentView {
+    struct Configuration: UIContentConfiguration {
+        var text: String? = ""
 
+        func makeContentView() -> UIView & UIContentView {
+            return TextFieldContentView(self)
+        }
+    }
+
+    let textField = UITextField()
+    var configuration: UIContentConfiguration {
+        didSet {
+            configure(configuration: configuration)
+        }
+    }
     override var intrinsicContentSize: CGSize {
         CGSize(width: 0, height: 44)
     }
 
-    init() {
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
         super.init(frame: .zero)
         addPinnedSubview(textField, insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
         textField.clearButtonMode = .whileEditing
@@ -23,5 +36,16 @@ class TextFieldContentView: UIView {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else { return }
+        textField.text = configuration.text
+    }
+}
+
+extension UICollectionViewListCell {
+    func textFieldConfiguration() -> TextFieldContentView.Configuration {
+        TextFieldContentView.Configuration()
     }
 }
---
+++ Today/Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Youssra Outelli on 15/03/2022.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+  func updated(for state: UIConfigurationState) -> Self {
+    return self
+  }
+}
